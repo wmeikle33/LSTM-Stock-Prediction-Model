1942dd72c09dcbc79d32fb6b02399757a7b50a4c
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-02-15 13:20:08 +0800

    Update model.py

--- src/stock_lstm/model.py
+++ src/stock_lstm/model.py
@@ -1,1 +1,9 @@
+model = keras.Sequential([
+    layers.Input(shape=(lookback, 1)),
+    layers.LSTM(64),
+    layers.Dense(1),
+])
+
+model.compile(optimizer="adam", loss="mse", metrics=["mae"])
+model.summary()
 
