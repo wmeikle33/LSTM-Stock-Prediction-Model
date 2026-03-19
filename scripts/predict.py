d9bf1a149590dded952751447dcb149414f774fe
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-19 19:34:25 +0800

    Update predict.py

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,4 +1,20 @@
-from stock_lstm.predict import main
+import argparse
+from stock_lstm.predict import predict_from_csv
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-dir", required=True)
+    parser.add_argument("--input", required=True)
+    parser.add_argument("--output", required=True)
+    args = parser.parse_args()
+
+    predict_from_csv(
+        model_dir=args.model_dir,
+        input_csv=args.input,
+        output_csv=args.output,
+    )
+
 
 if __name__ == "__main__":
     main()
