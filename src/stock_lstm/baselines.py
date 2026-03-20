eb404e7b430a2ee930cf7df89c0b5ca61bdfdc2e
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-21 00:15:26 +0800

    Update baselines.py

--- src/stock_lstm/baselines.py
+++ src/stock_lstm/baselines.py
@@ -3,5 +3,5 @@ import numpy as np
 def naive_last_close(y_prev_close):
     return y_prev_close
 
-def moving_average_baseline(close_windows):
+def moving_average(close_windows):
     return close_windows.mean(axis=1)
