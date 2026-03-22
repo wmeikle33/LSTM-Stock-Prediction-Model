870411fc5ed36c25a2ea3ac79a5c0f995c4f54ca
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-22 14:42:24 +0800

    Update generate_data.py

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -3,22 +3,26 @@ import numpy as np
 from pathlib import Path
 import argparse
 
-def generate_data(n, start_date, seed):
+def generate_data(input, n, start_date, seed):
     np.random.seed(seed)
 
     dates = pd.date_range(start=start_date, periods=n)
 
     returns = np.random.normal(0.0005, 0.02, n)
     price = 100 * np.exp(np.cumsum(returns))
-
-    df = pd.DataFrame({"date": dates})
-    df["close"] = price
-    df["open"] = df["close"].shift(1).fillna(df["close"])
-    df["high"] = df[["open", "close"]].max(axis=1) * (1 + np.random.rand(n) * 0.01)
-    df["low"] = df[["open", "close"]].min(axis=1) * (1 - np.random.rand(n) * 0.01)
-    df["volume"] = np.random.randint(1000, 10000, n)
-
-    return df[["date", "open", "high", "low", "close", "volume"]]
+    if input == 'train':
+        df = pd.DataFrame({"date": dates})
+        df["close"] = price
+        df["open"] = df["close"].shift(1).fillna(df["close"])
+        df["high"] = df[["open", "close"]].max(axis=1) * (1 + np.random.rand(n) * 0.01)
+        df["low"] = df[["open", "close"]].min(axis=1) * (1 - np.random.rand(n) * 0.01)
+        df["volume"] = np.random.randint(1000, 10000, n)
+        return df[["date", "open", "high", "low", "close", "volume"]]
+    elif input == 'test':
+        df = pd.DataFrame({"date": dates})
+        df["close"] = price
+        return df[["date", "close"]]
+        
 
 
 def main():
@@ -30,8 +34,10 @@ def main():
     parser.add_argument("--seed", type=int, default=42)
 
     args = parser.parse_args()
-
-    df = generate_data(args.n, args.start_date, args.seed)
+    if args.out == 'sample_data/prices_train.csv':
+        df = generate_data('train', args.n, args.start_date, args.seed)
+    elif args.out == 'sample_data/prices_test.csv':
+         df = generate_data('test', args.n, args.start_date, args.seed)
 
     out_path = Path(args.out)
     out_path.parent.mkdir(parents=True, exist_ok=True)
